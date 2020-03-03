9d172f7a2ccee50c330b8528bb88acb0aefbf132
Qing Chen <qing.chen@thoughtworks.com>
2020-03-03 16:47:38 +0800

    [TennisGame1][divergentVariation]extract Constants

---
+++ src/main/java/cn/xpbootcamp/tennis/Constants.java
@@ -0,0 +1,11 @@
+package cn.xpbootcamp.tennis;
+
+public class Constants {
+    public static final String LOVE = "Love";
+    public static final String FIFTEEN = "Fifteen";
+    public static final String THIRTY = "Thirty";
+    public static final String FORTY = "Forty";
+    public static final String DEUCE = "Deuce";
+    public static final String ADVANTAGE = "Advantage";
+    public static final String WIN = "Win";
+}
